0a850f35d99ba7c44124b7aa7e919d9a29ddcdfb
Pw-Wilfran <wilfranpilcue@gmail.com>
2025-04-26 23:16:50 -0500

    se crearon tablas con sus repectivas columnas y cardinalidades

---
+++ app/Http/Controllers/ApprenticeController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Apprentice;
+use Illuminate\Http\Request;
+
+class ApprenticeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Apprentice $apprentice)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Apprentice $apprentice)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Apprentice $apprentice)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Apprentice $apprentice)
+    {
+        //
+    }
+}
